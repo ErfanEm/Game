55298431909b3e7f0d0cb4ed64286547a79684d5
Erfan Emami <erfan.emami@eonerc.rwth-aachen.de>
2022-05-06 12:59:53 +0200

    First commit for login

---
+++ J.S.js
@@ -0,0 +1,20 @@
+
+function UserMinLength (x) {
+   let a = x.length;
+   if (a < 3){
+       return (alert('Username should have more than 2 characters'));
+   }
+   else{
+
+   }
+}
+
+function PassMinLength(x){
+    let a = x.length;
+    if (a < 8){
+        return (alert('Password should have more than 7 characters'));
+    }
+    else{
+        
+    }
+}
